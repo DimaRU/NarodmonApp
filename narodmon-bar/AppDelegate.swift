5c51319f6cc59db740168d4810258688241ebe74
Dmitriy Borovikov <dmitry@borovikov.name>
2017-11-14 18:02:22 +0300

    Initial Commit

---
+++ narodmon-bar/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  narodmon-bar
+//
+//  Created by Dmitriy Borovikov on 14.11.2017.
+//  Copyright © 2017 Dmitriy Borovikov. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
