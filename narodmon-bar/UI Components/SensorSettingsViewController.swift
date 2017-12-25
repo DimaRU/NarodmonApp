0f4ba031da5fc2b93281881e3ae53ac33ba2cc3d
Dmitriy Borovikov <dmitry@borovikov.name>
2017-12-25 11:35:28 +0300

    Sensors settings alfa-2

--- narodmon-bar/UI Components/SensorSettingsViewController.swift
+++ narodmon-bar/UI Components/SensorSettingsViewController.swift
@@ -93,34 +93,50 @@ class SensorSettingsViewController: NSViewController, NSTableViewDelegate, NSTab
         return false
     }
     
+    /// Start drag-n-drop
+    ///
+    /// - Returns: true if drag is allowed
     func tableView(_ tableView: NSTableView, writeRowsWith rowIndexes: IndexSet, to pboard: NSPasteboard) -> Bool {
         guard tableView == sensorsTableView || tableView == favoriteTableView else { return false }
+        let row = rowIndexes.first!
         switch tableView {
         case sensorsTableView:
             hasDraggedFavoriteItem = false
-            if targetDataArray.contains(rowIndexes.first!) {
+            if devicesSensorsList[row] is SensorsOnDevice {
+                return false
+            }
+            if targetDataArray.contains(row) {
                 return false
             }
         case favoriteTableView:
+            if row == 0 {
+                return false
+            }
             hasDraggedFavoriteItem = true
         default:
             return false
         }
 
         let data = NSKeyedArchiver.archivedData(withRootObject: rowIndexes)
-
         pboard.declareTypes([NSPasteboard.PasteboardType.string], owner: self)
         pboard.setData(data, forType: .string)
         
         return true
     }
 
+    func dragRow(from pasteboard: NSPasteboard) -> Int {
+        let rowData = pasteboard.data(forType: NSPasteboard.PasteboardType.string)!
+        let rowIndexes = NSKeyedUnarchiver.unarchiveObject(with: rowData) as! IndexSet
+        return rowIndexes.first!
+    }
+    
+    /// End drag-n-drop session without drop
+    ///  used for delete
+    ///
     func tableView(_ tableView: NSTableView, draggingSession session: NSDraggingSession, endedAt screenPoint: NSPoint, operation: NSDragOperation) {
         if tableView == favoriteTableView && (operation == .delete || currentItemDragOperation == .delete) {
-            let pboard = session.draggingPasteboard
-            let rowData = pboard.data(forType: NSPasteboard.PasteboardType.string)!
-            let rowIndexes = NSKeyedUnarchiver.unarchiveObject(with: rowData) as! IndexSet
-            targetDataArray.remove(at: rowIndexes.first!-1)
+            let row = dragRow(from: session.draggingPasteboard)
+            targetDataArray.remove(at: row-1)
             NSAnimationEffect.poof.show(centeredAt: screenPoint, size: NSZeroSize)
             favoriteTableView.reloadData()
         }
@@ -133,6 +149,9 @@ class SensorSettingsViewController: NSViewController, NSTableViewDelegate, NSTab
 //        return .every
 //    }
 
+    /// Determine valid drop target
+    ///
+    /// - Returns: operation
     func tableView(_ tableView: NSTableView, validateDrop info: NSDraggingInfo, proposedRow row: Int, proposedDropOperation dropOperation: NSTableView.DropOperation) -> NSDragOperation {
         currentItemDragOperation = []
         let toRow = row
@@ -140,43 +159,40 @@ class SensorSettingsViewController: NSViewController, NSTableViewDelegate, NSTab
         
         if tableView == favoriteTableView {
             tableView.setDropRow(toRow, dropOperation: .above)
-            
-            let data = info.draggingPasteboard().data(forType: .string)!
-            let rowIndexes = NSKeyedUnarchiver.unarchiveObject(with: data) as! IndexSet
-            let fromRow = rowIndexes.first!
+            let fromRow = dragRow(from: info.draggingPasteboard())
             
             if draggingSource == favoriteTableView {
                 currentItemDragOperation = toRow < 1 || toRow == fromRow || toRow == fromRow + 1 ? [] : .move
             }
             else if draggingSource == sensorsTableView {
                 currentItemDragOperation = .copy
-                //                currentItemDragOperation = toRow > 0 ? .copy : []
             }
         }
         return currentItemDragOperation
     }
 
+    /// Accept drop
+    ///
+    /// - Returns: true if drop accepted
     func tableView(_ tableView: NSTableView, acceptDrop info: NSDraggingInfo, row: Int, dropOperation: NSTableView.DropOperation) -> Bool {
         guard tableView == sensorsTableView || tableView == favoriteTableView else { return false }
-        let data = info.draggingPasteboard().data(forType: .string)!
-        let rowIndexes = NSKeyedUnarchiver.unarchiveObject(with: data) as! IndexSet
+        let fromRow = dragRow(from: info.draggingPasteboard())
 
        switch info.draggingSource() as? NSTableView {
         case favoriteTableView?:
-            let toRow = rowIndexes.first! - 1
-            let value = targetDataArray[toRow]
-            targetDataArray.remove(at: toRow)
-            if row > targetDataArray.count {
-                targetDataArray.insert(value, at: row-1)
+            let value = targetDataArray[fromRow-1]
+            targetDataArray.remove(at: fromRow-1)
+            if (row-1) > targetDataArray.count {
+                targetDataArray.insert(value, at: row-2)
             }
             else {
-                targetDataArray.insert(value, at: row)
+                targetDataArray.insert(value, at: row-1)
             }
             favoriteTableView.reloadData()
             return true
             
         case sensorsTableView?:
-            targetDataArray.append(rowIndexes.first!)
+            targetDataArray.append(fromRow)
             favoriteTableView.reloadData()
             return true
             
