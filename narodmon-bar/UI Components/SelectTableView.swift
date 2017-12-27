ecc0c3890f864c0688d6cc7b19225907b9d807f0
Dmitriy Borovikov <dmitry@borovikov.name>
2017-12-27 22:25:48 +0300

    SelectTableView refactoring

--- narodmon-bar/UI Components/SelectTableView.swift
+++ narodmon-bar/UI Components/SelectTableView.swift
@@ -23,34 +23,30 @@ class SelectTableView: NSTableView {
     }
     
     override func draggingSession(_ session: NSDraggingSession, movedTo screenPoint: NSPoint) {
-        //[super draggingSession:session movedToPoint:screenPoint];
         guard let viewController = delegate as? SensorSettingsViewController else { return }
 
-        let windowPoint: NSPoint? = window?.mouseLocationOutsideOfEventStream
-        let localPoint = convert(windowPoint ?? NSPoint(), from: nil)
+        let windowPoint = window!.mouseLocationOutsideOfEventStream
+        let localPoint = convert(windowPoint , from: nil)
         if viewController.currentItemDragOperation != .delete && viewController.hasDraggedFavoriteItem && mouse(localPoint, in: NSInsetRect(visibleRect, -35, -35)) == false {
             viewController.currentItemDragOperation = .delete
         }
         else if viewController.currentItemDragOperation == .delete && viewController.hasDraggedFavoriteItem && mouse(localPoint, in: NSInsetRect(visibleRect, -35, -35)) == true {
             viewController.currentItemDragOperation = []
         }
-        if viewController.currentItemDragOperation == .delete {
+        
+        switch viewController.currentItemDragOperation {
+        case .delete:
             NSCursor.disappearingItem.set()
             session.animatesToStartingPositionsOnCancelOrFail = false
-        }
-        else if viewController.currentItemDragOperation == .private {
+        case .private:
             NSCursor.operationNotAllowed.set()
             session.animatesToStartingPositionsOnCancelOrFail = true
-        }
-        else if viewController.currentItemDragOperation == .copy {
+        case .copy:
             NSCursor.dragCopy.set()
             session.animatesToStartingPositionsOnCancelOrFail = false
-        }
-        else {
+        default:
             NSCursor.closedHand.set()
             session.animatesToStartingPositionsOnCancelOrFail = true
         }
-        
     }
-
 }
