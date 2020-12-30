10f10fa0bd04a26c2522ee389b059ae15eceb2f9
Dmitriy Borovikov <dmitry@borovikov.name>
2020-12-30 10:23:22 +0300

    send more information about fatal errors

--- NarodmonApp/ErrorReport.swift
+++ NarodmonApp/ErrorReport.swift
@@ -12,8 +12,7 @@ import PromiseKit
 extension Error {
     
     func sendFatalReport() -> Never {
-        print(self)
-        let errorDescription = self.localizedDescription
+        let errorDescription = String(reflecting: self)
         let errorDetail: String
         
         if case let e as NarodNetworkError = self {
@@ -32,7 +31,7 @@ extension Error {
         _ = NarProvider.shared.request(.sendReport(message: errorDescription, logs: errorDetail))
         
         usleep(200000)      // 0.2 sec for send Req
-        fatalError(errorDescription)
+        fatalError(self.localizedDescription)
     }
 
 }
